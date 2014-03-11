2cb19f997a0a4445664360ae5c8ecf0f56e46842
andot <mabingyao@gmail.com>
2014-03-11 18:15:41 +0800

    Fixed some Notices.

--- php5/HproseReader.php
+++ php5/HproseReader.php
@@ -74,16 +74,16 @@ class HproseReader extends HproseRawReader {
         $tag = $this->stream->getc();
         $result = NULL;
         switch ($tag) {
-            case '0': return 0;
-            case '1': return 1;
-            case '2': return 2;
-            case '3': return 3;
-            case '4': return 4;
-            case '5': return 5;
-            case '6': return 6;
-            case '7': return 7;
-            case '8': return 8;
-            case '9': return 9;
+            case '0': $result = 0; break;
+            case '1': $result = 1; break;
+            case '2': $result = 2; break;
+            case '3': $result = 3; break;
+            case '4': $result = 4; break;
+            case '5': $result = 5; break;
+            case '6': $result = 6; break;
+            case '7': $result = 7; break;
+            case '8': $result = 8; break;
+            case '9': $result = 9; break;
             case HproseTags::TagInteger: $result = $this->readIntegerWithoutTag(); break;
             case HproseTags::TagLong: $result = $this->readLongWithoutTag(); break;
             case HproseTags::TagDouble: $result = $this->readDoubleWithoutTag(); break;
