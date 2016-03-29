1fd9cf7af8ed0b410fc813e4110417d72b473a76
Ma Bingyao <mabingyao@gmail.com>
2016-03-29 23:23:02 +0800

    Update .travis.yml

---
+++ install_ext.sh
@@ -0,0 +1,19 @@
+if [[ $TRAVIS_PHP_VERSION = '5.3' ]]; then
+  wget https://pecl.php.net/get/libevent-0.1.0.tgz
+  tar zxvfp libevent-0.1.0.tgz
+  cd libevent-0.1.0
+  phpize
+  ./configure
+  make
+  make install
+  echo "extension = libevent.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+elif [[ $TRAVIS_PHP_VERSION != 'hhvm' ]]; then
+  git clone https://bitbucket.org/osmanov/pecl-event.git
+  cd pecl-event
+  phpize
+  ./configure --with-event-core --with-event-extra --enable-event-debug
+  make
+  make install
+  echo "extension = event.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+fi
+cd ..
