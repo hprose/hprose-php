b90f5355254a4b0793d2fa40c1dd8a989cb81cb1
Ma Bingyao <mabingyao@gmail.com>
2016-03-30 23:33:19 +0800

    Update install_ext.sh

--- install_ext.sh
+++ install_ext.sh
@@ -9,14 +9,6 @@ if [[ $TRAVIS_PHP_VERSION = '5.3' ]]; then
   echo "extension = libevent.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 elif [[ $TRAVIS_PHP_VERSION = 'hhvm' ]]; then
   sudo apt-get install -y hhvm-dev
-  sudo apt-get install -y libgmp-dev
-  sudo apt-get install -y libmpfr4 libmpfr-dev
-  sudo apt-get install -y libmpc-dev libmpc2
-  sudo apt-get install -y libtool
-  sudo apt-get install -y m4
-  sudo apt-get install -y bison
-  sudo apt-get install -y flex
-  sudo apt-get install -y autoconf
   sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   sudo apt-get update
   sudo apt-get install -y gcc-4.8 g++-4.8 gcc-4.8-multilib g++-4.8-multilib
@@ -24,9 +16,6 @@ elif [[ $TRAVIS_PHP_VERSION = 'hhvm' ]]; then
   sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
   sudo update-alternatives --config gcc
   sudo update-alternatives --config g++
-  sudo apt-get update
-  sudo apt-get upgrade -y
-  sudo apt-get dist-upgrade -y
   git clone https://github.com/chobie/hhvm-uv.git --recursive
   cd hhvm-uv
   make -C libuv CFLAGS=-fPIC
