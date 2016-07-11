7f74c6a0e9e1917d345986ecc88f61cb866e8be2
Ma Bingyao <mabingyao@gmail.com>
2016-07-11 16:34:42 +0800

    Added Future/Wrapper

---
+++ src/Hprose/Future/Wrapper.php
@@ -0,0 +1,47 @@
+<?php
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+
+/**********************************************************\
+ *                                                        *
+ * Hprose/Future/Wrapper.php                              *
+ *                                                        *
+ * Future Wrapper for php 5.3+                            *
+ *                                                        *
+ * LastModified: Jul 10, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose\Future {
+    class Wrapper {
+        protected $obj;
+        public function __construct($obj) {
+            $this->obj = $obj;
+        }
+        public function __call($name, array $arguments) {
+            $method = array($this->obj, $name);
+            return all($arguments)->then(function($args) use ($method) {
+                return call_user_func_array($method, $args);
+            });
+        }
+        public function __get($name) {
+            return $this->obj->$name;
+        }
+        public function __set($name, $value) {
+            $this->obj->$name = $value;
+        }
+        public function __isset($name) {
+            return isset($this->obj->$name);
+        }
+        public function __unset($name) {
+            unset($this->obj->$name);
+        }
+    }
+}
