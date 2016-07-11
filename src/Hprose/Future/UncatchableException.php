d0876e96155fd1a54c2823d21ae4c49ed4dee70d
Ma Bingyao <mabingyao@gmail.com>
2016-07-11 16:34:25 +0800

    Added UncatchableException

---
+++ src/Hprose/Future/UncatchableException.php
@@ -0,0 +1,24 @@
+<?php
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+
+/**********************************************************\
+ *                                                        *
+ * Hprose/Future/UncatchableException.php                 *
+ *                                                        *
+ * UncatchableException for php 5.3+                      *
+ *                                                        *
+ * LastModified: Jul 10, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose\Future {
+    class UncatchableException extends \Exception {}
+}
