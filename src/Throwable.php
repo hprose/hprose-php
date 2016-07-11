a1a44cd994f3b6e5cfc46615ac179465b9aab91f
Ma Bingyao <mabingyao@gmail.com>
2016-07-11 16:33:38 +0800

    Added Throwable for PHP7-

---
+++ src/Throwable.php
@@ -0,0 +1,35 @@
+<?php
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+
+/**********************************************************\
+ *                                                        *
+ * Throwable.php                                          *
+ *                                                        *
+ * Throwable for php 7-                                   *
+ *                                                        *
+ * LastModified: Jul 11, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace {
+    if (!interface_exists("Throwable")) {
+        interface Throwable {
+            public function getMessage();
+            public function getCode();
+            public function getFile();
+            public function getLine();
+            public function getTrace();
+            public function getTraceAsString();
+            public function getPrevious();
+            public function __toString();
+        }
+    }
+}
