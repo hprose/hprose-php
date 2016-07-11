8d1927eba1fe7d8f10fd4bb7eab2173e97a2bce8
Ma Bingyao <mabingyao@gmail.com>
2016-07-11 16:34:53 +0800

    Added Future/CallableWrapper

---
+++ src/Hprose/Future/CallableWrapper.php
@@ -0,0 +1,31 @@
+<?php
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+
+/**********************************************************\
+ *                                                        *
+ * Hprose/Future/CallableWrapper.php                      *
+ *                                                        *
+ * Future CallableWrapper for php 5.3+                    *
+ *                                                        *
+ * LastModified: Jul 11, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace Hprose\Future {
+    class CallableWrapper extends Wrapper {
+        public function __invoke() {
+            $obj = $this->obj;
+            return all(func_get_args())->then(function($args) use ($obj) {
+                return call_user_func_array($obj, $args);
+            });
+        }
+    }
+}
