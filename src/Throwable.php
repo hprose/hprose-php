a550d017c5cf3113cfe7e85022a445ae1752c95f
Ma Bingyao <mabingyao@gmail.com>
2016-07-11 16:56:06 +0800

    Update Throwable

--- src/Throwable.php
+++ src/Throwable.php
@@ -19,17 +19,15 @@
  *                                                        *
 \**********************************************************/
 
-namespace {
-    if (!interface_exists("Throwable")) {
-        interface Throwable {
-            public function getMessage();
-            public function getCode();
-            public function getFile();
-            public function getLine();
-            public function getTrace();
-            public function getTraceAsString();
-            public function getPrevious();
-            public function __toString();
-        }
+if (!interface_exists("Throwable")) {
+    interface Throwable {
+        public function getMessage();
+        public function getCode();
+        public function getFile();
+        public function getLine();
+        public function getTrace();
+        public function getTraceAsString();
+        public function getPrevious();
+        public function __toString();
     }
 }
