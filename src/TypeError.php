f569bee884903933c7ba1273fcf796b7408cdf8c
Ma Bingyao <mabingyao@gmail.com>
2016-07-11 16:33:51 +0800

    Added TypeError for PHP7-

---
+++ src/TypeError.php
@@ -0,0 +1,26 @@
+<?php
+/**********************************************************\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: http://www.hprose.com/                 |
+|                   http://www.hprose.org/                 |
+|                                                          |
+\**********************************************************/
+
+/**********************************************************\
+ *                                                        *
+ * TypeError.php                                          *
+ *                                                        *
+ * TypeError for php 7-                                   *
+ *                                                        *
+ * LastModified: Jul 11, 2016                             *
+ * Author: Ma Bingyao <andot@hprose.com>                  *
+ *                                                        *
+\**********************************************************/
+
+namespace {
+    if (!class_exists('TypeError')) {
+        class TypeError extends Exception {}
+    }
+}
