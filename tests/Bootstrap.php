ce620395849e14d6bdcb7056b499e473685ae7d6
Christopher CHEN <chenshaoming@appgame.com>
2017-05-22 13:27:51 +0800

    test: fix phpunit 6 compatibility issue
    close #41

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,2 +1,8 @@
 <?php
 $loader = include __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+
+if (!class_exists(PHPUnit_Framework_TestCase::class) && class_exists(TestCase::class)) {
+    class_alias(TestCase::class, PHPUnit_Framework_TestCase::class);
+}
