2195db8124ddeb2847de893e67b5f789cd0613f6
andot <mabingyao@gmail.com>
2020-02-01 13:44:43 +0800

    Update MockTest.php

--- tests/MockTest.php
+++ tests/MockTest.php
@@ -12,48 +12,42 @@ class MockTest extends PHPUnit_Framework_TestCase {
         $service->addCallable(function ($name) {
             return 'hello ' . $name;
         }, 'hello');
-        $server = new MockServer('test');
+        $server = new MockServer('testHelloWorld');
         $service->bind($server);
-        $client = new Client(['mock://test']);
+        $client = new Client(['mock://testHelloWorld']);
         $proxy = $client->useService();
         $result = $proxy->hello('world');
         $this->assertEquals($result, 'hello world');
         $server->close();
     }
     public function testClientTimeout() {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('timeout');
         $service = new Service();
         $service->addCallable(function ($time) {
             sleep($time);
         }, 'wait');
-        $server = new MockServer('test');
+        $server = new MockServer('testClientTimeout');
         $service->bind($server);
-        $client = new Client(['mock://test']);
+        $client = new Client(['mock://testClientTimeout']);
         $client->timeout = 1000;
         $proxy = $client->useService();
-        try {
-            $proxy->wait(2);
-            $this->assertEquals('ok', 'fuck');
-        } catch (Exception $e) {
-            $this->assertEquals($e->getMessage(), 'timeout');
-        }
+        $proxy->wait(2);
         $server->close();
     }
     public function testServiceTimeout() {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('timeout');
         $service = new Service();
         $service->addCallable(function ($time) {
             sleep($time);
         }, 'wait');
         $service->use([new ExecuteTimeoutHandler(1000), 'handler']);
-        $server = new MockServer('test');
+        $server = new MockServer('testServiceTimeout');
         $service->bind($server);
-        $client = new Client(['mock://test']);
+        $client = new Client(['mock://testServiceTimeout']);
         $proxy = $client->useService();
-        try {
-            $proxy->wait(2);
-            $this->assertEquals('ok', 'fuck');
-        } catch (Exception $e) {
-            $this->assertEquals($e->getMessage(), 'timeout');
-        }
+        $proxy->wait(2);
         $server->close();
     }
     public function testMissingMethod() {
@@ -61,9 +55,9 @@ class MockTest extends PHPUnit_Framework_TestCase {
         $service->addMissingMethod(function (string $name, array $args): string {
             return $name . json_encode($args);
         });
-        $server = new MockServer('test');
+        $server = new MockServer('testMissingMethod');
         $service->bind($server);
-        $client = new Client(['mock://test']);
+        $client = new Client(['mock://testMissingMethod']);
         $proxy = $client->useService();
         $result = $proxy->hello('world');
         $this->assertEquals($result, 'hello["world"]');
