798b27dbdb11a8be4581a7a3d23e14412573b9b7
andot <mabingyao@gmail.com>
2020-02-16 19:41:48 +0800

    Update Hprose.php

--- src/Hprose.php
+++ src/Hprose.php
@@ -7,8 +7,6 @@
 |                                                          |
 |  Hprose.php                                              |
 |                                                          |
-|  Hprose for PHP 7.1+                                     |
-|                                                          |
 |  LastModified: Jun 7, 2019                               |
 |  Author: Ma Bingyao <andot@hprose.com>                   |
 |                                                          |
