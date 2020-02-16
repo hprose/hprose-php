574bcd5030d146b7855545212f8ddbce66359912
andot <mabingyao@gmail.com>
2020-02-16 19:42:51 +0800

    Update Singleton.php

--- src/Hprose/RPC/Core/Singleton.php
+++ src/Hprose/RPC/Core/Singleton.php
@@ -7,8 +7,6 @@
 |                                                          |
 | Hprose/RPC/Core/Singleton.php                            |
 |                                                          |
-| Singleton for PHP 7.1+                                   |
-|                                                          |
 | LastModified: Jun 7, 2019                                |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
