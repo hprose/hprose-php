3b0659eaee834c44786a0cd87b2251c5e706dfd2
andot <mabingyao@gmail.com>
2020-02-16 19:43:17 +0800

    Update MockAgent.php

--- src/Hprose/RPC/Mock/MockAgent.php
+++ src/Hprose/RPC/Mock/MockAgent.php
@@ -7,8 +7,6 @@
 |                                                          |
 | Hprose/RPC/Mock/MockAgent.php                            |
 |                                                          |
-| Hprose MockAgent for PHP 7.1+                            |
-|                                                          |
 | LastModified: Feb 1, 2020                                |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
