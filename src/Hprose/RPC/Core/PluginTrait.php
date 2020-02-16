c3ec08fb5744656e1fb9ae2ae0b5a5ef2b0c1b5e
andot <mabingyao@gmail.com>
2020-02-16 19:42:37 +0800

    Update PluginTrait.php

--- src/Hprose/RPC/Core/PluginTrait.php
+++ src/Hprose/RPC/Core/PluginTrait.php
@@ -7,8 +7,6 @@
 |                                                          |
 | Hprose/RPC/Core/PluginTrait.php                          |
 |                                                          |
-| PluginTrait for PHP 7.1+                                 |
-|                                                          |
 | LastModified: Feb 10, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
