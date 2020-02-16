c64d601518cb127694a5d81ee3122ad68f57cafd
andot <mabingyao@gmail.com>
2020-02-16 19:42:54 +0800

    Update TimeoutException.php

--- src/Hprose/RPC/Core/TimeoutException.php
+++ src/Hprose/RPC/Core/TimeoutException.php
@@ -7,8 +7,6 @@
 |                                                          |
 | Hprose/RPC/Core/TimeoutException.php                     |
 |                                                          |
-| Hprose TimeoutException for PHP 7.1+                     |
-|                                                          |
 | LastModified: Jun 7, 2019                                |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
