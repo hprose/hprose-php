e4d246d7e40f440f70993a38a5c024d9bed6131e
andot <mabingyao@gmail.com>
2020-02-16 19:42:05 +0800

    Update DefaultClientCodec.php

--- src/Hprose/RPC/Core/DefaultClientCodec.php
+++ src/Hprose/RPC/Core/DefaultClientCodec.php
@@ -7,8 +7,6 @@
 |                                                          |
 | Hprose/RPC/Core/DefaultClientCodec.php                   |
 |                                                          |
-| Hprose DefaultClientCodec for PHP 7.1+                   |
-|                                                          |
 | LastModified: Feb 1, 2020                                |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
