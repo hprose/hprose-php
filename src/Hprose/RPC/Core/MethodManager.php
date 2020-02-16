8ce7cb26ee825fc9655f2bfef8681b14632b2c84
andot <mabingyao@gmail.com>
2020-02-16 19:42:26 +0800

    Update MethodManager.php

--- src/Hprose/RPC/Core/MethodManager.php
+++ src/Hprose/RPC/Core/MethodManager.php
@@ -7,8 +7,6 @@
 |                                                          |
 | Hprose/RPC/Core/MethodManager.php                        |
 |                                                          |
-| Hprose MethodManager for PHP 7.1+                        |
-|                                                          |
 | LastModified: Jan 24, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
