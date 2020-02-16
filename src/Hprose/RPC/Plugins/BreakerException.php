1c07e2fb4ed84bb94131a67f9a7b5ed8a504474b
andot <mabingyao@gmail.com>
2020-02-16 23:36:04 +0800

    Create BreakerException.php

---
+++ src/Hprose/RPC/Plugins/BreakerException.php
@@ -0,0 +1,19 @@
+<?php
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| Hprose/RPC/Plugins/BreakerException.php                  |
+|                                                          |
+| LastModified: Feb 16, 2020                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+namespace Hprose\RPC\Plugins;
+
+use Exception;
+
+class BreakerException extends Exception {}
