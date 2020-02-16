5ea85a721e3d5f9902a4dc10b3506e21bf2a3e88
andot <mabingyao@gmail.com>
2020-02-16 21:47:47 +0800

    Create NginxRoundRobinLoadBalance.php

---
+++ src/Hprose/RPC/Plugins/NginxRoundRobinLoadBalance.php
@@ -0,0 +1,59 @@
+<?php
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| Hprose/RPC/Plugins/NginxRoundRobinLoadBalance.php        |
+|                                                          |
+| LastModified: Feb 16, 2020                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+namespace Hprose\RPC\Plugins;
+
+use Hprose\RPC\Core\Context;
+use Throwable;
+
+class NginxRoundRobinLoadBalance extends WeightedLoadBalance {
+    private $effectiveWeights;
+    private $currentWeights;
+    public function __construct(array $uriList) {
+        parent::__construct($uriList);
+        $this->effectiveWeights = $this->weights;
+        $this->currentWeights = array_fill(0, count($this->uris), 0);
+    }
+    public function handler(string $request, Context $context, callable $next): string {
+        $n = count($this->uris);
+        $index = -1;
+        $totalWeight = array_sum($this->effectiveWeights);
+        if ($totalWeight > 0) {
+            $currentWeight = log(0);
+            for ($i = 0; $i < $n; ++$i) {
+                $weight = ($this->currentWeights[$i] += $this->effectiveWeights[$i]);
+                if ($currentWeight < $weight) {
+                    $currentWeight = $weight;
+                    $index = $i;
+                }
+            }
+            $this->currentWeights[$index] = $currentWeight - $totalWeight;
+        } else {
+            $index = random_int(0, $n - 1);
+        }
+        $context->uri = $this->uris[$index];
+        try {
+            $response = $next($request, $context);
+            if ($this->effectiveWeights[$index] < $this->weights[$index]) {
+                ++$this->effectiveWeights[$index];
+            }
+            return $response;
+        } catch (Throwable $e) {
+            if ($this->effectiveWeights[$index] > 0) {
+                --$this->effectiveWeights[$index];
+            }
+            throw $e;
+        }
+    }
+}
