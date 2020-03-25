2d0b22eef5a77164abf63bdaeffd1a8e743aaad6
andot <mabingyao@gmail.com>
2020-03-25 08:29:52 +0800

    Update ErrorLevel.php

--- src/Hprose/RPC/Core/ErrorLevel.php
+++ src/Hprose/RPC/Core/ErrorLevel.php
@@ -22,13 +22,14 @@ trait ErrorLevel {
         E_COMPILE_ERROR => "Fatal Error",
         E_USER_ERROR => "Fatal Error",
         E_WARNING => "Warning",
+        E_CORE_WARNING => "Warning",
         E_USER_WARNING => "Warning",
         E_COMPILE_WARNING => "Warning",
         E_RECOVERABLE_ERROR => "Warning",
         E_NOTICE => "Notice",
         E_USER_NOTICE => "Notice",
         E_STRICT => "Strict",
-        E_DEPRECATED => "Strict",
+        E_DEPRECATED => "Deprecated",
         E_USER_DEPRECATED => "Deprecated",
     ];
 }
